ec7d95c721b8972a082f889357640a31fd32c980
Sferratore <sferratore.francescopaolo00@gmail.com>
2023-11-04 16:23:18 +0100

    Added TakingInput exercise

---
+++ 5. TakingInput/TakingInput.py
@@ -0,0 +1,13 @@
+print("How old are you? ")
+
+age = input() # Console waits for you to write a value and enter it, so that it will be memorized inside the variable "age".
+
+print("How tall are you? ")
+
+height = input()
+
+print("How much do you weigh? ")
+
+weight = input()
+
+print(f"Ah, so you are {age} years old, you are {height} tall and you weigh {weight}.")
