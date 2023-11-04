b7ceb993446b285fac33f9ca74a7ed4d4f7991f7
Sferratore <sferratore.francescopaolo00@gmail.com>
2023-11-04 17:55:48 +0100

    Added Functions exercise.

---
+++ 8. Functions/Functions.py
@@ -0,0 +1,12 @@
+# Using def keyword to define functions.
+
+def print_args(arg1, arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+def print_helloworld():
+    print("Hello, world!")
+
+# Using the functions.
+
+print_helloworld()
+print_args("Hello,", "world!")
