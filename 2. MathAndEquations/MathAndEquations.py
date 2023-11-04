aa4c04425ce59413d9ca89cbd54b69d4988946bd
Sferratore <sferratore.francescopaolo00@gmail.com>
2023-11-04 15:59:13 +0100

    Added MathAndEquations exercise

---
+++ 2. MathAndEquations/MathAndEquations.py
@@ -0,0 +1,11 @@
+30+20 # This is a normal math operation in python.
+
+print(50/5) # Operations can be printed with the retrieved results.
+
+print("Now we will see the result of 11*12:", 11*12) # Operations can also be appended to a string, there is automatic conversion.
+
+5 != 10 # This is an example of boolean comparison. In this case we can see an equation which will return "true" if it is in fact true and "false" otherwise.
+
+print(11*2 == 22 * 1) # We can also print the result of it.
+
+print(31 >= 10*4) # Another example of boolean comparison.
