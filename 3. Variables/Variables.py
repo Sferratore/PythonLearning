c86ea36c6bf6ca6e47b2b0346f1dbc8c0f1294ad
Sferratore <sferratore.francescopaolo00@gmail.com>
2023-11-04 16:07:07 +0100

    Added Variables exercise

---
+++ 3. Variables/Variables.py
@@ -0,0 +1,7 @@
+cats = 4 # In Python we can use variables. You can see variables as containers of information. Being python not a typed language, the types of variables are implicit.
+
+dogs = 5
+
+animals = cats + dogs # You can perform operations with the values contained in the variables.
+
+print(animals)
