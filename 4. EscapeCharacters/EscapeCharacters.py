d31b9a437e945b87f05d94ebf97a0cf956bc1699
Sferratore <sferratore.francescopaolo00@gmail.com>
2023-11-04 16:17:32 +0100

    Added EscapeCharacters exercise

---
+++ 4. EscapeCharacters/EscapeCharacters.py
@@ -0,0 +1,7 @@
+# Escape characters are used in the strings to trigger certain events.
+
+print("I'm split\nOn two lines") # \n goes to the new line.
+
+print("\tI'm tabbed in") # \t gives a tab of space. 
+
+print("It's time to quote something without going out of the string. Try \"To be or not to be\"!") # \" lets you use the double quotes without breaking the string. 
